d4923558005c7d170981493d5eca1c9614607707
SaiNageswarS <sainageswar@gmail.com>
2016-11-01 21:41:15 +0530

    changing to absolute paths

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-./gradlew build
+/home/ec2-user/CognitiveGuru/gradlew build
 killall java
-nohup java -Djava.security.egd=file:/dev/./urandom -jar ./build/libs/CognitiveGuru-0.0.1-SNAPSHOT.jar &
+nohup java -Djava.security.egd=file:/dev/./urandom -jar /home/ec2-user/CognitiveGuru/build/libs/CognitiveGuru-0.0.1-SNAPSHOT.jar &
