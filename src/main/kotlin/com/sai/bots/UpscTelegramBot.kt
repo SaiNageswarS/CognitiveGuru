d6504ce675fdef322672dbbfc4db5659fc40149e
SaiNageswarS <sainageswar@gmail.com>
2016-11-05 01:04:05 +0530

    added concurrency to message handling

--- src/main/kotlin/com/sai/bots/UpscTelegramBot.kt
+++ src/main/kotlin/com/sai/bots/UpscTelegramBot.kt
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Message
 import org.telegram.telegrambots.api.objects.Update
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import java.util.concurrent.Executors
 
 /**
  * Created by sainageswar on 16/10/16.
@@ -20,27 +21,32 @@ class UpscTelegramBot (
         @Autowired val messageHandlers: MessageHandlers
     ): TelegramLongPollingBot() {
 
+    // creating a thread pool of 10 threads
+    private val exec = Executors.newFixedThreadPool(10)
+
     override fun getBotUsername(): String = upscBotUsername
 
     override fun getBotToken(): String = upscBotToken
 
     override fun onUpdateReceived(update: Update?) {
         if(update != null && update.hasMessage()){
-            val message: Message = update.getMessage();
+            exec.submit {
+                val message: Message = update.getMessage();
 
-            val cgUser: CgUser? = botDataServices.getCgUser(message.from)
+                val cgUser: CgUser? = botDataServices.getCgUser(message.from)
 
-            val sendMessageRequest: SendMessage = when {
-                message.isReply ->
-                    messageHandlers.handleReply(message, cgUser)
-                cgUser == null ->
-                    messageHandlers.handleNoUser(message)
-                message.isCommand ->
-                    messageHandlers.handleCommand(message)
-                else ->
-                    messageHandlers.handleEchoMessage(message)
+                val sendMessageRequest: SendMessage = when {
+                    message.isReply ->
+                        messageHandlers.handleReply(message, cgUser)
+                    cgUser == null ->
+                        messageHandlers.handleNoUser(message)
+                    message.isCommand ->
+                        messageHandlers.handleCommand(message)
+                    else ->
+                        messageHandlers.handleEchoMessage(message)
+                }
+                sendMessage(sendMessageRequest)
             }
-            sendMessage(sendMessageRequest)
         }
     }
 }
