6ee261ea62b576fde5f2967c8aaa190ff088ac73
SaiNageswarS <sainageswar@gmail.com>
2016-12-31 18:27:09 +0530

    capturing intents in webhook request

--- src/main/kotlin/com/sai/beans/ApiAIResult.kt
+++ src/main/kotlin/com/sai/beans/ApiAIResult.kt
@@ -8,10 +8,13 @@ import com.fasterxml.jackson.annotation.JsonInclude
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-class ApiAIResult {
-    var source: String? = null
-    var resolvedQuery: String? = null
-    var speech: String? = null
-    var action: String? = null
-    var actionIncomplete: Boolean? = null
-}
+class ApiAIResult (
+    var source: String? = null,
+    var resolvedQuery: String? = null,
+    var speech: String? = null,
+    var action: String? = null,
+    var actionIncomplete: Boolean? = null,
+    var parameters: Map<String, Any> = mapOf(),
+    var contexts: List<Context> = listOf(),
+    var metadata: Map<String, Any> = mapOf()
+)
