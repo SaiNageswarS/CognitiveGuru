a79915332fbfd19f2d1ff56bad3851a7e07d1347
SaiNageswarS <sainageswar@gmail.com>
2017-01-01 21:32:43 +0530

    removing rubenugulus telegram client

--- src/main/kotlin/com/sai/beans/chatClients/TelegramApiAIRequest.kt
+++ src/main/kotlin/com/sai/beans/chatClients/TelegramApiAIRequest.kt
@@ -3,12 +3,34 @@ package com.sai.beans.chatClients
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.sai.beans.OriginalRequest
-import org.telegram.telegrambots.api.objects.Update
 
 /**
  * Created by sainageswar on 30/12/16.
  */
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class TelegramFrom (
+    var id: Int = 0,
+    var first_name: String = "",
+    var last_name: String = "",
+    var username: String = ""
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class TelegramMessage (
+        var message_id: Int = 0,
+        var from: TelegramFrom = TelegramFrom()
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class TelegramUpdate (
+    var update_id: String = "",
+    var message: TelegramMessage = TelegramMessage()
+)
+
 class TelegramApiAIRequest (
-    var data: Update = Update()
+    var data: TelegramUpdate = TelegramUpdate()
 ): OriginalRequest()
